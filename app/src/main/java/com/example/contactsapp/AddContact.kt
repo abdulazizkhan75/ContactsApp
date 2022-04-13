c1d3f70dcd16dd65a236b11590e9ce05ef351d9e
Ryan Dreise <47092780+HiddenElement@users.noreply.github.com>
2022-04-13 14:30:39 -0400

    Fix Issues/add google maps
    - Fixed file parsing issues with duplicate code/files
    - Added image for app icon
    - Added ability to open google maps with entered user address

--- app/src/main/java/com/example/contactsapp/AddContact.kt
+++ app/src/main/java/com/example/contactsapp/AddContact.kt
@@ -6,26 +6,13 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.Switch
 import kotlin.random.Random
-
-class AddContact : AppCompatActivity() {
-    lateinit var btn_cancel: Buttonpackage com.example.contactsapp
-
 import android.content.Intent
 import android.net.Uri
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
-import android.widget.Switch
 import androidx.core.view.isVisible
-import kotlin.random.Random
 
 class AddContact : AppCompatActivity() {
     lateinit var btn_cancel: Button
     lateinit var btn_done: Button
-
-
-
     lateinit var fnameEditText: EditText
     lateinit var lnameEditText: EditText
     lateinit var isFavourite: Switch
@@ -34,6 +21,7 @@ class AddContact : AppCompatActivity() {
     lateinit var emailEditText: EditText
     lateinit var btn_call: Button
     lateinit var btn_email: Button
+    lateinit var btn_maps: Button
     lateinit var birthdayEditText: EditText
     lateinit var addressEditText: EditText
     lateinit var notesEditText: EditText
@@ -52,6 +40,7 @@ class AddContact : AppCompatActivity() {
         emailEditText = findViewById(R.id.editText_Email)
         btn_call = findViewById(R.id.button_call)
         btn_email = findViewById(R.id.button_email)
+        btn_maps = findViewById(R.id.button_OpenMaps)
         birthdayEditText = findViewById(R.id.editText_Birthday)
         addressEditText = findViewById(R.id.editText_homeAddress)
         notesEditText = findViewById(R.id.editText_notes)
@@ -60,6 +49,7 @@ class AddContact : AppCompatActivity() {
 
         btn_call.isVisible = false
         btn_email.isVisible = false
+        btn_maps.isVisible = false
 
         btn_cancel.setOnClickListener {
             finish()
@@ -75,9 +65,16 @@ class AddContact : AppCompatActivity() {
             notesEditText.setText(intent.getStringExtra("notes"))
             isFavourite.isChecked = intent.getBooleanExtra("isFavourite", false)
             isEmergency.isChecked = intent.getBooleanExtra("isEmergency", false)
-            btn_call.isVisible = true
-            btn_email.isVisible = true
 
+            if (phoneNumberEditText.text.isNotEmpty()) {
+                btn_call.isVisible = true
+            }
+            if (emailEditText.text.isNotEmpty()) {
+                btn_email.isVisible = true
+            }
+            if (addressEditText.text.isNotEmpty()) {
+                btn_maps.isVisible = true
+            }
         }
 
         btn_call.setOnClickListener{
@@ -86,11 +83,9 @@ class AddContact : AppCompatActivity() {
 
                 val callIntent: Intent = Intent(Intent.ACTION_DIAL, Uri.parse("tel:$number"))
                 startActivity(callIntent)
-
-
-
             }
         }
+
 
         btn_email.setOnClickListener{
             if(intent.getBooleanExtra("ShowContact", false)){
@@ -100,101 +95,15 @@ class AddContact : AppCompatActivity() {
                 startActivity(emailIntent)
             }
         }
-
-        btn_done.setOnClickListener {
-            if (!intent.getBooleanExtra("ShowContact", false)) {
-                contactsDB.ContactDao().insert(
-                    Contacts(
-                        0,
-                        fnameEditText.text.toString(),
-                        lnameEditText.text.toString(),
-                        emailEditText.text.toString(),
-                        phoneNumberEditText.text.toString(),
-                        birthdayEditText.text.toString(),
-                        addressEditText.text.toString(),
-                        notesEditText.text.toString(),
-                        isFavourite.isChecked, isEmergency.isChecked,
-                        Random.nextInt(5 - 1) + 1
-                    )
-                )
-            }else {
-                if (fnameEditText.text.toString() != intent.getStringExtra("fname")){
-                    contactsDB.ContactDao().updateFname(fnameEditText.text.toString(), intent.getIntExtra("ID", -1))
-                }
-                if (lnameEditText.text.toString() != intent.getStringExtra("lname")){
-                    contactsDB.ContactDao().updatelname(lnameEditText.text.toString(), intent.getIntExtra("ID", -1))
-                }
-                if (emailEditText.text.toString() != intent.getStringExtra("email")){
-                    contactsDB.ContactDao().updateEmail(emailEditText.text.toString(), intent.getIntExtra("ID", -1))
-                }
-                if (phoneNumberEditText.text.toString() != intent.getStringExtra("phoneNumber")){
-                    contactsDB.ContactDao().updatePhoneNumber(phoneNumberEditText.text.toString(), intent.getIntExtra("ID", -1))
-                }
-                if (birthdayEditText.text.toString() != intent.getStringExtra("bdate")){
-                    contactsDB.ContactDao().updateBirthday(birthdayEditText.text.toString(), intent.getIntExtra("ID", -1))
-                }
-                if (addressEditText.text.toString() != intent.getStringExtra("address")){
-                    contactsDB.ContactDao().updateAddress(addressEditText.text.toString(), intent.getIntExtra("ID", -1))
-                }
-                if (notesEditText.text.toString() != intent.getStringExtra("notes")){
-                    contactsDB.ContactDao().updateNotes(notesEditText.text.toString(), intent.getIntExtra("ID", -1))
-                }
-                if (isFavourite.isChecked != intent.getBooleanExtra("isFavourite", false)){
-                    contactsDB.ContactDao().updateIsFavourite(isFavourite.isChecked, intent.getIntExtra("ID", -1))
-                }
-                if (isEmergency.isChecked != intent.getBooleanExtra("isEmergency", false)){
-                    contactsDB.ContactDao().updateIsEmergency(isEmergency.isChecked, intent.getIntExtra("ID", -1))
-                }
+        btn_maps.setOnClickListener {
+            if(intent.getBooleanExtra("ShowContact", false)){
+                var address = addressEditText.text.toString() //Get entered address for contact
+                address = address.replace(" ", "+")
+                val gmmIntentUri = Uri.parse("geo:0,0?q=$address")
+                val mapIntent = Intent(Intent.ACTION_VIEW, gmmIntentUri)
+                mapIntent.setPackage("com.google.android.apps.maps")
+                startActivity(mapIntent)
             }
-            finish()
-        }
-    }
-}
-    lateinit var btn_done: Button
-
-    lateinit var fnameEditText: EditText
-    lateinit var lnameEditText: EditText
-    lateinit var isFavourite: Switch
-    lateinit var isEmergency: Switch
-    lateinit var phoneNumberEditText: EditText
-    lateinit var emailEditText: EditText
-    lateinit var birthdayEditText: EditText
-    lateinit var addressEditText: EditText
-    lateinit var notesEditText: EditText
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_contact)
-
-        val contactsDB = ContactsDatabase.getInstance(applicationContext)
-
-        btn_cancel = findViewById(R.id.button_cancel)
-        btn_done = findViewById(R.id.button_done)
-        fnameEditText = findViewById(R.id.editText_fname)
-        lnameEditText = findViewById(R.id.editText_lname)
-        phoneNumberEditText = findViewById(R.id.editText_PhoneNumber)
-        emailEditText = findViewById(R.id.editText_Email)
-        birthdayEditText = findViewById(R.id.editText_Birthday)
-        addressEditText = findViewById(R.id.editText_homeAddress)
-        notesEditText = findViewById(R.id.editText_notes)
-        isFavourite = findViewById(R.id.switch_isFavourite)
-        isEmergency = findViewById(R.id.switch_isEmergency)
-
-        btn_cancel.setOnClickListener {
-            finish()
-        }
-
-        if (intent.getBooleanExtra("ShowContact", false)){
-            fnameEditText.setText(intent.getStringExtra("fname"))
-            lnameEditText.setText(intent.getStringExtra("lname"))
-            emailEditText.setText(intent.getStringExtra("email"))
-            phoneNumberEditText.setText(intent.getStringExtra("phoneNumber"))
-            birthdayEditText.setText(intent.getStringExtra("bdate"))
-            addressEditText.setText(intent.getStringExtra("address"))
-            notesEditText.setText(intent.getStringExtra("notes"))
-            isFavourite.isChecked = intent.getBooleanExtra("isFavourite", false)
-            isEmergency.isChecked = intent.getBooleanExtra("isEmergency", false)
-
         }
 
         btn_done.setOnClickListener {
